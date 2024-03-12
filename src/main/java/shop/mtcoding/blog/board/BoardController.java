5d08971bb170965d4500dfb0e3127569560f9ceb
dongkipark1 <qkrehdrl4417@naver.com>
2024-03-12 10:56:41 +0900

    컨트롤러 의존성 주입

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,12 +1,24 @@
-package shop.mtcoding.blog.controller;
+package shop.mtcoding.blog.board;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class BoardController {
+
+    private final BoardNativeRepository boardNativeRepository;
+
+    @PostMapping("/board/save")
+    public String save(String title, String content, String username){
+        boardNativeRepository.save(title,content,username);
+
+        return "redirect:/";
+    }
 
     @GetMapping("/")
     public String index() {
@@ -15,7 +27,7 @@ public class BoardController {
 
     @GetMapping("/board/save-form")
     public String saveForm() {
-        return "save-form";
+        return "/board/save-form";
     }
 
     // 래핑 클래스를 쓰면 null이 들어오는 지 확인이 가능함(null 처리가 편함)
