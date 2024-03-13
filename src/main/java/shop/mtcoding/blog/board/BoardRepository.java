77085861e1e490c2460b824e5c622416def18308
dongkipark1 <qkrehdrl4417@naver.com>
2024-03-13 16:49:46 +0900

    게시글 상세보기 ORM 사용

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -1,6 +1,7 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BoardRepository {
     private final EntityManager em;
+
+    public Board findByIdJoinUser(int id){
+        Query query = em.createQuery("select b from Board b join fetch b.user u where b.id = :id", Board.class);
+        query.setParameter("id" , id);
+        return (Board) query.getSingleResult();
+    }
 
     public Board findById(int id){
         // id, title, content, user_id(이질감 생김), created_at
--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -14,10 +14,21 @@ public class BoardRepositoryTest {
     private BoardRepository boardRepository;
 
     @Test
-    public void findById_test(){
+    public void findByIdJoinUser(){
         int id = 1;
 
-        boardRepository.findById(id);
+        Board board = boardRepository.findByIdJoinUser(id);
+    }
+
+    @Test
+    public void findById_test(){
+        int id = 1;
+        System.out.println("start - 1");
+        Board board = boardRepository.findById(id);
+        System.out.println("start - 2");
+        System.out.println(board.getUser().getId());
+        System.out.println("start - 3");
+        System.out.println(board.getUser().getUsername());
 
 //        Board board = new Board(1, "제목1", "내용", "날짜");
 
