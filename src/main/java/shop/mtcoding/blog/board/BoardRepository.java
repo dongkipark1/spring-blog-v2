4f3d0658e1e9b6f66345d5d4e50e4cafc1439a3c
dongkipark1 <qkrehdrl4417@naver.com>
2024-03-14 12:52:12 +0900

    for문과 foreach문 사용, stream을 통해

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -4,13 +4,70 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
 public class BoardRepository {
     private final EntityManager em;
+
+    public List<Board> findAllV3(){
+        String q1 = "select b from Board b order by b.id desc";
+        List<Board> boardList = em.createQuery(q1 , Board.class).getResultList();
+
+        int[] userIds = boardList.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+
+        String q2 = "select u from User u where u.id in (";
+        for (int i = 0; i < userIds.length ; i++) {
+            if (i == userIds.length - 1){
+                q2 = q2 + userIds[i] + ")";
+            }else {
+                q2 = q2 + userIds[i] + ",";
+            }
+        }
+        List<User> userList = em.createQuery(q2 , User.class).getResultList();
+
+        for (Board board : boardList){
+            for (int i = 0; i < userList.size(); i++) {
+                User user = userList.get(i);
+                if (user.getId() == board.getUser().getId()){
+                    board.setUser(user);
+                }
+            }
+        }
+
+        return boardList; // user가 채워져 있어야함.
+    }
+
+    public List<Board> findAllV2(){
+        Query q1 = em.createQuery("select b from Board b order by b.id desc", Board.class);
+        List<Board> boardList = q1.getResultList();
+
+        List<Integer> userIds = boardList.stream().mapToInt(value -> value.getUser().getId()).distinct().boxed().toList();
+
+        Query q2 = em.createQuery("select u from User u where u.id IN :userIds", User.class);
+        q2.setParameter("userIds", userIds);
+        List<User> userList = q2.getResultList();
+
+        boardList.stream().forEach(b -> {
+            User user = userList.stream().filter(u -> u.getId() == b.getUser().getId()).findFirst().get();
+        });
+        return boardList; // user가 채워져 있어야 한다.
+    }
+
+    //        String q2 = "select u from User u where u.id in (";
+//        q2 = q2 + ":id" + 1 + ",";
+//        q2 = q2 + ":id" + 2 + ",";
+//        q2 = q2 + ":id" + 3 + ")";
+//        Query query2 = em.createQuery(q2, User.class);
+//        query2.setParameter("id1", 3);
+//        query2.setParameter("id2", 2);
+//        query2.setParameter("id3", 1);
+
 
     public List<Board> findAll(){
         Query query = em.createQuery("select b from Board b order by b.id desc" , Board.class);
--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -16,6 +16,45 @@ public class BoardRepositoryTest {
     private BoardRepository boardRepository;
 
     @Test
+    public void findAllV2_test(){
+        List<Board> boardList = boardRepository.findAllV2();
+        System.out.println("findAllV2_test : 조회완료 쿼리 2번");
+        boardList.forEach(board -> {
+            System.out.println(board);
+        });
+    }
+
+    @Test
+    public void randomquery_test(){
+        int[] ids = {1,2};
+
+        // select u from User u where u.id in (?,?);
+        String q = "select u from User u where u.id in (";
+        for (int i=0; i<ids.length; i++){
+            if(i==ids.length-1){
+                q = q + "?)";
+            }else{
+                q = q + "?,";
+            }
+        }
+        System.out.println(q);
+    }
+
+    @Test
+    public void findAll_custom_inquery_test(){
+        //given
+
+        //when
+        List<Board> boardList = boardRepository.findAll();
+
+        int[] usedIds = boardList.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+        for (int i : usedIds){
+            System.out.println(i);
+        }
+        //then
+    }
+
+    @Test
     public void findAll_lazyloading_test(){
         //given
 
