f8bdd6125528d824b665fc4850d380ca460a9351
dongkipark1 <qkrehdrl4417@naver.com>
2024-03-19 15:57:53 +0900

    댓글 삭제

--- src/main/java/shop/mtcoding/blog/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyService.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Transient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
 import shop.mtcoding.blog._core.errors.exception.Exception404;
 import shop.mtcoding.blog.board.Board;
 import shop.mtcoding.blog.board.BoardJPARepository;
@@ -25,5 +26,16 @@ public class ReplyService {
         replyJPARepository.save(reply);
     }
 
+    @Transactional
+    public void 댓글삭제(Integer replyId, Integer sessionUserId) {
+        Reply reply = replyJPARepository.findById(replyId)
+                .orElseThrow(() -> new Exception404("댓글을 찾을 수 없음"));
+
+        if (sessionUserId != reply.getUser().getId()){
+            throw new Exception403("댓글 삭제 권한 없음");
+        }
+
+        replyJPARepository.deleteById(replyId);
+    }
 
 }
